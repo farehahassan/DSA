3b63968e75d3020c7fdae9c20a5afb1746df9d66
Fareha <hfariha104@gmail.com>
2023-05-29 22:29:07 +0500

    Update

--- BinarySearchh.java
+++ BinarySearchh.java
@@ -1,36 +1,91 @@
-public class BinarySearchh {
-    public static int binarySearch(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
+// public class BinarySearchh {
+//     public static int binarySearch(int[] arr, int target) {
+//         int low = 0;
+//         int high = arr.length - 1;
         
-        while (low <= high) {
-            int mid = (low + high) / 2;
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
             
-            if (arr[mid] == target) {
+//             if (arr[mid] == target) {
+//                 return mid;
+//             }
+            
+//             if (arr[mid] < target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+        
+//         return -1;
+//     }
+    
+//     // Example usage
+//     public static void main(String[] args) {
+//         int[] numbers = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+//         int targetNumber = 11;
+        
+//         int result = binarySearch(numbers, targetNumber);
+        
+//         if (result != -1) {
+//             System.out.println("Element found at index " + result);
+//         } else {
+//             System.out.println("Element not found in the list.");
+//         }
+//     }
+// }
+
+
+// revision 
+
+public class BinarySearchh{
+    public static int binarySearch(int[] data , int target){
+        int low = 0;
+        int high = data.length -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            
+            if(data[mid] == target){
                 return mid;
             }
-            
-            if (arr[mid] < target) {
+            if(data[mid]< target){
                 low = mid + 1;
-            } else {
-                high = mid - 1;
+            }
+            else {
+                high = mid -1;
             }
         }
-        
+
+
         return -1;
     }
-    
-    // Example usage
+
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
-        int targetNumber = 11;
+        int[] data = {2 , 4 , 1, 6 ,14, 11 , 12};
+        int target = 22;
+
+       int result =  binarySearch(data, target);
+
+       if(result != -1){
+        System.out.println("Item found at location " + result);
+       }
+
+       else{
+        System.out.println("Item dosn't found");
+       }
         
-        int result = binarySearch(numbers, targetNumber);
-        
-        if (result != -1) {
-            System.out.println("Element found at index " + result);
-        } else {
-            System.out.println("Element not found in the list.");
-        }
     }
 }
+
+
+
+/*  Binary Search theory
+ *
+ * -->It follows a divide-and-conquer approach by repeatedly dividing the search space in half until the target element is found or determined to be absent.
+ * --> Binary search has a time complexity of O(log n)
+ * --> If the target element is equal to the middle element, the search is successful.
+ * --> If the target element is less than the middle element, the search continues in the lower half of the search space.
+ * --> If the target element is greater than the middle element, the search continues in the upper half of the search space.
+ * --> This process of halving the search space is repeated until the target element is found or the search space is empty. 
+ */
